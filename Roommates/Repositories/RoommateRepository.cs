d3b41549936670c6be78415e8c46c021105b717d
Sara Hackle <Shackle@ivytech.edu>
2022-03-17 09:56:41 -0500

    Created Roommates, Rooms, and Chores

---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName, RentPortion, Name FROM Roommate LEFT JOIN Room ON RoomId = Room.Id WHERE Roommate.Id = @id ";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+                        if (reader.Read())
+                        {
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room = new Room { Name = reader.GetString(reader.GetOrdinal("Name")) }
+                            };
+                        }
+                        return roommate;
+                    }
+                }
+            }
+        }
+    }
+}
