4c0270435991347973be3d385c1f1ff83598acca
Sara Hackle <Shackle@ivytech.edu>
2022-03-17 10:11:17 -0500

    Rooms, Roommates, Chores created

--- Roommates/Models/Room.cs
+++ Roommates/Models/Room.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Roommates.Models
+﻿namespace Roommates.Models
 {
     // C# representation of the Room table
     public class Room
